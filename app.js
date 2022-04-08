3694c2bd870021b5fb955f7e0614c4e2cb9a9f68
Matthew Parker <mattparker124@gmail.com>
2022-04-08 05:41:40 -0500

    added eslint and inquirer

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ const [name, github] = profileDataArgs;
 
 fs.writeFile('./index.html', generatePage(name, github), err => {
     if (err) throw err;
-  
+
     console.log('Portfolio complete! Check out index.html to see the output!');
-  });
+});
